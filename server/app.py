635e671da18e5c5df0561792ccf738807d249ccb
Seth David Gyimah <esec26@gmail.com>
2023-10-01 18:36:31 +0000

    completed session/cookies lab

--- server/app.py
+++ server/app.py
@@ -20,15 +20,52 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
+
+
 @app.route('/articles')
 def index_articles():
 
-    pass
+    articles = Article.query.all()
+    articles_dict = [article.to_dict() for article in articles]
+    response = make_response(jsonify(articles_dict), 200)
+    return response
 
+
+
+# View Function to limit user article view to 3
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    # step 1: if user viewing for first time, set counter to 0 or get current counter value
+    session['page_views'] = session.get('page_views', 0)
+
+    # step 2: increase the value of counter by 1
+    session['page_views'] += 1
+
+    # step 3: check if counter is more than 3, render error
+    if session['page_views'] > 3:
+        response = make_response(
+            {'message': 'Maximum pageview limit reached'},
+            401
+        )
+        return response
+    
+    # else if counter is not more than 3, attempt to fetch and serve article using Article ID
+    article = Article.query.filter(Article.id == id).first()
+
+    if article:
+        response = make_response(
+            jsonify(article.to_dict()),
+            200
+        )
+        return response
+    else:
+        response = make_response(
+            {'message': 'Article not found'},
+            200
+        )
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
